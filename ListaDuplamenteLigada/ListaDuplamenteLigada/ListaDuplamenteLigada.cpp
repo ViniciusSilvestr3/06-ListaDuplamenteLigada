5e27bf5316b38391d98c37390adf996449390cd2
Vinicius Silvestre <viniciusilvestre98@gmail.com>
2025-04-08 12:00:53 -0300

    Funções devidamente aplicadas

--- ListaDuplamenteLigada/ListaDuplamenteLigada/ListaDuplamenteLigada.cpp
+++ ListaDuplamenteLigada/ListaDuplamenteLigada/ListaDuplamenteLigada.cpp
@@ -150,17 +150,60 @@ void inserirElemento()
 // funções a serem implementadas no exericio
 void exibirReverso()
 {
-
+	if (ultimo == NULL) {
+		cout << "Lista vazia \n";
+		return;
+	}
+	else {
+		cout << "Elementos na ordem reversa: \n";
+		NO* aux = ultimo;
+		while (aux != NULL) {
+			cout << aux->valor << endl;
+			aux = aux->ant;
+		}
+	}
 }
 
 void excluirPrimeiroElemento()
 {
+	if (primeiro == NULL) {
+		cout << "Lista vazia \n";
+		return;
+	}
 
+	NO* paraExcluir = primeiro;
+	primeiro = primeiro->prox;
+
+	if (primeiro != NULL) {
+		primeiro->ant = NULL;
+	}
+	else {
+		ultimo = NULL; 
+	}
+
+	free(paraExcluir);
+	cout << "Primeiro elemento excluido \n";
 }
 
 void excluirUltimoElemento()
 {
+	if (ultimo == NULL) {
+		cout << "Lista vazia \n";
+		return;
+	}
 
+	NO* paraExcluir = ultimo;
+	ultimo = ultimo->ant;
+
+	if (ultimo != NULL) {
+		ultimo->prox = NULL;
+	}
+	else {
+		primeiro = NULL;
+	}
+
+	free(paraExcluir);
+	cout << "Ultimo elemento excluido \n";
 }
 
 
